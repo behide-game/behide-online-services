acfe2478274e084a31d4ec2fe5b822c36f3aabab
Gaël Bouquain <gael.bouquain@gmail.com>
2024-06-10 23:09:15 +0200

    Retrieve the google id_token

--- src/Behide.OnlineServices/Program.fs
+++ src/Behide.OnlineServices/Program.fs
@@ -9,6 +9,7 @@ open Falco.HostBuilder
 open System.Text.Json.Serialization
 open Microsoft.AspNetCore.Builder
 open Microsoft.Extensions.DependencyInjection
+open Microsoft.AspNetCore.Authentication.Cookies
 
 
 let configureServices (services: IServiceCollection) =
@@ -23,6 +24,19 @@ let configureServices (services: IServiceCollection) =
     services.AddSingleton<Signaling.IConnAttemptStore, Signaling.ConnAttemptStore>() |> ignore
     services.AddSingleton<Signaling.IRoomStore, Signaling.RoomStore>() |> ignore
     services.AddSingleton<Signaling.IPlayerConnsStore, Signaling.PlayerConnsStore>() |> ignore
+
+    // services
+    //     .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    //     .AddCookie(fun options ->
+    //         options.LoginPath <- "/auth/sign-in"
+    //         options.LogoutPath <- "/auth/sign-out"
+    //     )
+    //     .AddGoogle("google", fun options ->
+    //         options.ClientId <- ""
+    //         options.ClientSecret <- ""
+    //         options.CallbackPath <- "/auth/signin-google"
+    //     )
+    // |> ignore
 
 let appBuilder (app: IApplicationBuilder) =
     app.UseRouting() |> ignore
@@ -39,8 +53,12 @@ let main args =
             )
         )
         use_middleware appBuilder
+
+        // use_authentication
+        // use_authorization
         endpoints [
             get "/" (Response.ofPlainText "Hello world")
+            yield! Auth.endpoints
         ]
     }
     0
